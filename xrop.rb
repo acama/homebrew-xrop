40edd7ca116abf604d06abdbc6538c6d45ddc8fc
acez <amatcama@gmail.com>
2024-08-16 21:42:43 +0000

    initial commit

---
+++ xrop.rb
@@ -0,0 +1,19 @@
+class Xrop < Formula
+  desc "ROP Gadget finder "
+  homepage "https://github.com/acama/xrop"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version "1.2"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/acama/homebrew-xrop/releases/download/v1.2/xrop-aarch64-darwin.tgz"
+    sha256 "991765877cff3ea19a5073a0ec443d1da36bb5e9ea40b5daf4d8f60f46ddb5bc"
+  else
+    url ""
+    sha256 ""
+  end
+
+  def install
+    (prefix/).install Dir["./*"]
+    Dir.glob(prefix/"xrop") {|file| bin.install_symlink file}
+  end
+end
